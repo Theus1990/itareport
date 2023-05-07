a254d76bd7f9946513f62d8e3e8c06dffc8ff428
Eric Mesquita <ericmesqui2468@gmail.com>
2023-05-07 17:54:00 -0300

    footer added

---
+++ itareport/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+            <h1>Footer é pra ser aqui</h1>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
--- itareport/src/components/header/Header.jsx
+++ itareport/src/components/header/Header.jsx
@@ -1,19 +1,19 @@
+import './Header.css'
+
 function Header(){
     return (
-        <div className="App">
-              <div className="main">
-                <div className="center">
-                    <div className="menu">
-                      <div className="logo">
-                         <h1>ItaReport</h1>
-                      </div>
-                      <div className="item-menu">
-                        <a href="#">Cadastrar</a>
-                      </div>  
-                    </div>
-                </div>
-              </div>
+      <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div className="logo">
+                  <h1>ItaReport</h1>
+            </div>
+            <div className="item-menu">
+              <a href="#">Cadastrar</a>
+            </div>  
+          </div>
         </div>
+      </div>
       )
 
 }
