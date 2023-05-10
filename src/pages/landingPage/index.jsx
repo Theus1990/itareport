c3b7bd1f605bebee04710fc580790f24f469890e
Eric Mesquita <ericmesqui2468@gmail.com>
2023-05-10 16:56:10 -0300

    register form added

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -5,28 +5,30 @@ import "./style.css"
 
 export default function LandingPage() {
     return (
-        <div>
-            <Header />
-            <div>
-                <div className='Desc'>
-                    <h2>Faça uma denúncia, faça a diferença!</h2> <br />
-                    <p>
-                        Colabore com outros cidadãos e ajude a tornar nossa
-                        cidade um lugar melhor!
-                    </p>
-                </div>
-                <div className='CTA'>
-                    <div className='CTA-Bottom d-flex flex-column m-auto bg-info'>
-                        <a
-                            href='/'
-                            className='d-flex p-10 text-center text-decoration-none'
-                        >
-                            Ver Mapa de Denúncias
-                        </a>
+        <>
+            <div className='landingpage'>
+                <Header />
+                <div>
+                    <div className='Desc'>
+                        <h2>Faça uma denúncia, faça a diferença!</h2> <br />
+                        <p>
+                            Colabore com outros cidadãos e ajude a tornar nossa
+                            cidade um lugar melhor!
+                        </p>
+                    </div>
+                    <div className='CTA'>
+                        <div className='CTA-Bottom d-flex flex-column m-auto bg-info'>
+                            <a
+                                href='/'
+                                className='d-flex p-10 text-center text-decoration-none'
+                            >
+                                Ver Mapa de Denúncias
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
             <Footer />
-        </div>
+        </>
     )
 }
