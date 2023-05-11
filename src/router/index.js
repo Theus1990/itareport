57820b3a65e5474ffcc083cad69608325a839ae7
Theus1990 <matheusrabelo9@hotmail.com>
2023-05-10 23:03:15 -0300

    Criação da página de Denúncia

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landingPage";
 import Register from "../pages/register";
+import Forms from "../pages/forms";
 
-
-export default function Routing(){
+export default function Routing() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage/>} />
-      <Route path="register/" element={<Register />}/>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="register/" element={<Register />} />
+      <Route path="forms/" element={<Forms />} />
     </Routes>
   );
 }
