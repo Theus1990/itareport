3263589c957f301e12c5d4eef57eb0b25060c4ba
Lucas Sousa <lucassousa28@alu.ufc.br>
2023-06-18 21:55:25 -0300

    cards centralized, hover added at cta bottom, title text bold

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,73 +11,54 @@ export default function LandingPage() {
             <div className='landingpage'>
                 <div className="text-center text-2xl">
                     <div className='Desc'>
-                        <h2 className="text-3xl pt-10">Faça uma denúncia, faça a diferença!</h2> <br />
+                        <h2 className="text-4xl pt-10 font-bold">Faça uma denúncia, faça a diferença!</h2> <br />
                         <p className="pb-10">
                             Colabore com outros cidadãos e ajude a tornar nossa
                             cidade um lugar melhor!
                         </p>
                     </div>
                     <div className='CTA'>
-                        <div className='CTA-Bottom'>
-                            <div className="BG-CTA-Bottom card-img-top flex justify-center items-center">
+                            <div className="BG-CTA-Bottom card-img-top border-solid flex justify-center items-center">
                                  <Image src={cta} className='cta-img' alt='...' />
                                  <a
-                                href='/mapa/'
-                                className='rounded border-black border-2 bg-gray-500 text-gray-200 p-4 absolute'
-                            >
-                                Ver Mapa de Denúncias
-                            </a>
+                                    href='/mapa/'
+                                    className='cta-bottom rounded border-black border-2 bg-gray-500 text-gray-200 p-4 absolute'
+                                 >Ver Mapa de Denúncias
+                                 </a>
                             </div>
-                           
-                        </div>
                     </div>
                 </div>
                 <div className='grid grid-cols-3 gap-4 pt-10'>
                     <div className='card border-solid  border-black border-2 p-2'>
                         <Image src={cta} className='card-img-top border-solid  border-black border-2' alt='...' />
-                        <div className='card-body text-light'>
-                            <h5 className='card-title'>Card title</h5>
-                            <p className='card-text'>
+                        <div className='card-body text-center'>
+                            <h5 className='card-title text-center text-3xl'>1º Passo</h5>
+                            <p className='card-text text-2xl'>
                                 This is a wider card with supporting text below
                                 as a natural lead-in to additional content. This
                                 content is a little bit longer.
                             </p>
-                            <p className='card-text'>
-                                <small className='text-body-secondary'>
-                                    Last updated 3 mins ago
-                                </small>
-                            </p>
                         </div>
                     </div>
                     <div className='card border-solid  border-black border-2 p-2'>
                         <Image src={cta} className='card-img-top border-solid  border-black border-2' alt='...' />
-                        <div className='card-body text-light'>
-                            <h5 className='card-title'>Card title</h5>
-                            <p className='card-text'>
+                        <div className='card-body text-center'>
+                            <h5 className='card-title text-center text-3xl'>2º Passo</h5>
+                            <p className='card-text text-2xl'>
                                 This card has supporting text below as a natural
                                 lead-in to additional content.
                             </p>
-                            <p className='card-text'>
-                                <small className='text-body-secondary'>
-                                    Last updated 3 mins ago
-                                </small>
-                            </p>
                         </div>
                     </div>
                     <div className='card border-solid  border-black border-2 p-2'>
                         <Image src={cta} className='card-img-top border-solid  border-black border-2' alt='...' />
-                        <div className='card-body text-light'>
-                            <h5 className='card-title'>Card title</h5>
-                            <p className='card-text'>
+                        <div className='card-body text-center'>
+                            <h5 className='card-title text-center text-3xl'>3º Passo</h5>
+                            <p className='card-text text-2xl'>
                                 This is a wider card with supporting text below
                                 as a natural lead-in to additional content. This
                                 card has even longer content than the first to
                                 show that equal height action.
-                            </p>
-                            <p className='card-text'>
-                                <small className='text-body-secondary'>
-                                    Last updated 3 mins ago
-                                </small>
                             </p>
                         </div>
                     </div>
