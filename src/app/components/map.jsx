b3bc96649a07d4b69ed5e7ba99ad3ac5d2fe6344
Lucas Sousa <lucassousa28@alu.ufc.br>
2023-07-08 20:00:16 -0300

    current location added

--- src/app/components/map.jsx
+++ src/app/components/map.jsx
@@ -1,39 +1,65 @@
 "use client"
 
 import "leaflet/dist/leaflet.css"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 export default function Map() {
-    const position = [-3.6836, -39.5816]
-    const pin = L.icon({
-        iconUrl: "pinmap.svg",
-        iconSize: [32, 32],
-        iconAnchor: [24, 48],
-        popupAnchor: [0, -48]
-    })
 
-    return (
-        <MapContainer
-            center={position}
-            zoom={13}
-            scrollWheelZoom={true}
-            minZoom={3}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            <Marker position={position} icon={pin}>
+    function LocationMarker() {
+        const [position, setPosition] = useState(null);
+        const [bbox, setBbox] = useState([]);
+
+        const pin = L.icon({
+            iconUrl: "pinmap.svg",
+            iconSize: [32, 32],
+            iconAnchor: [16, 16],
+            popupAnchor: [0, -48]
+        })
+    
+        const map = useMap();
+    
+        useEffect(() => {
+          map.locate().on("locationfound", function (e) {
+            setPosition(e.latlng);
+            map.flyTo(e.latlng, map.getZoom());
+            const radius = e.accuracy;
+            const circle = L.circle(e.latlng, radius);
+            circle.addTo(map);
+            setBbox(e.bounds.toBBoxString().split(","));
+          });
+        }, [map]);
+
+        return position === null ? null : (
+             <Marker position={position} icon={pin}>
                 <Popup
                     closeButton={false}
                     minWidth={240}
                     maxWidth={240}
                     className='map-popup'
-                >
+                >   
                     <h3>title</h3>
                     <p>description</p>
                 </Popup>
             </Marker>
+          );
+        }
+
+    return (
+        <MapContainer
+            center={[-3.6906281127994665, -39.57835150669607]}
+            zoom={13}
+            scrollWheelZoom
+            style={{ height: "100vh" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />  
+          <LocationMarker />
         </MapContainer>
     )
+
 }
--- src/app/mapa/page.jsx
+++ src/app/mapa/page.jsx
@@ -3,15 +3,21 @@
 import Header from "../components/header"
 import Footer from "../components/footer"
 import dynamic from "next/dynamic"
+import React, { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const DynamicMap = dynamic(() => import("../components/map"), { ssr: false })
 
 export default function Mapa() {
-    return (
+      return (
         <>
-            <Header />
-            <DynamicMap />
-            <Footer />
+        <Header />
+        <DynamicMap />
+        <Footer />
         </>
-    )
+      );
+ 
+   
 }
